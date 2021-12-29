e144b0a2bd84d5ee6bc79cce881e10ff10902c26
ajad2 <ajad9223@gmail.com>
2021-12-29 13:21:22 +0530

    space added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,27 +8,27 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function UserDetails()
-    {
-        $data = [];
-        $data['name'] = 'rahul';
-        // $data['number'] = '8025415694';
-        $data['email'] = 'rahul@gmail.com';
-        $data['password'] = 'rahul@1234';
+  public function UserDetails()
+  {
+    $data = [];
+    $data['name'] = 'rahul';
+    // $data['number'] = '8025415694';
+    $data['email'] = 'rahul@gmail.com';
+    $data['password'] = 'rahul@1234';
 
-        //  $data['age'] = '27';
-        // dd($data);
-        $user  = User::create($data);
-    }
-    public function updateUser()
-    {
+    //  $data['age'] = '27';
+    // dd($data);
+    $user  = User::create($data);
+  }
+  public function updateUser()
+  {
 
-       // $affect = User::where('id', 1)->update(['name' => 'darshan']);
-        //dd($affect);
-        $affect = User::where('id', '1')->first ();
-          tap($affect)->update(
-            ['email' => 'dharshan@gmail.com']);
-            dd($affect->toSql());
-
-    }
+    // $affect = User::where('id', 1)->update(['name' => 'darshan']);
+    //dd($affect);
+    $affect = User::where('id', '1')->first();
+    tap($affect)->update(
+      ['email' => 'dharshan@gmail.com']
+    );
+    dd($affect->toSql());
+  }
 }
